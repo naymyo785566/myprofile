ee0cbf5b7ab9d3f2f85953b9dd5f17a15a254980
BMPS <naymyo785566@gamil.com>
2023-05-14 19:17:09 +0630

    my portfolio

---
+++ js/script.js
@@ -0,0 +1,64 @@
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
+
+// Sticky Navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In The View Port
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+// More Detail to Download CV button
+const btnMoreDetail = document.querySelector(".btn--more-detail");
+btnMoreDetail.addEventListener("click", function (e) {
+  e.preventDefault();
+  const id = btnMoreDetail.getAttribute("href");
+  console.log(id);
+  document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+});
+
+// Text Writing Animation
+const text = document.querySelector(".sec-text");
+const textload = () => {
+  setTimeout(() => {
+    text.textContent = "Web Developer";
+  }, 0);
+  setTimeout(() => {
+    text.textContent = "Web Designer";
+  }, 4000);
+  setTimeout(() => {
+    text.textContent = "Freelancer";
+  }, 8000);
+};
+setInterval(textload, 12000);
